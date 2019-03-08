6d73ce85551517ca95129bea6b5f16758f887f25
Douglas H. King <dhking@wharton.upenn.edu>
2019-03-08 16:57:37 -0500

    Initial Commit, version 0.1

---
+++ README.md
@@ -0,0 +1,46 @@
+# Settlers of Concat
+
+## Synopsis
+A zero player game to illustrate AWS Step Functions. Using [SFN](https://aws.amazon.com/step-functions/) and [lambda](https://aws.amazon.com/lambda/) Services this is essentially a tutorial. It is based on popular tabletop game mechanics, but that is really just a point of familiarity to make it easier to digest. My work is in research programming, and this came out of a project where we used Step Functions to solve a data audit process requirement. In order to help promote the use of the processes outlined here, we needed a simple framework that could be used as a catalyst for learning methods of the Step Function.
+
+
+## Repository 
+
+[https://github.com/douglascodes/settlers-of-concat](https://github.com/douglascodes/settlers-of-concat)
+
+
+## Requirements 
+
+ - AWS Services Account
+ - Python 3.6+
+ - Browser
+ - Git
+ - Text editor
+ - Patience
+ - Understand of [AWS IAM](https://aws.amazon.com/iam/)
+
+## Build
+
+There is no "build" since it is a loosely connected set of Lambda functions and other AWS services orchestrated in AWS SFN.
+
+
+## Tests
+
+TBD
+
+
+## Reference
+
+ - [AWS Step Functions Tutorial](https://docs.aws.amazon.com/step-functions/latest/dg/welcome.html)
+ - [AWS Lambda Getting Started](https://aws.amazon.com/lambda/getting-started/)
+ - 
+
+## Contributors
+
+(Douglas H. King)[https://github.com/douglascodes]
+
+## Version History
+
+### Version 0.1
+ - Lambda function for rolling die.
+ - Wrapper Step Function
---
+++ lambdas/settlers_roll_die.py
@@ -0,0 +1,61 @@
+import json
+import re
+import random
+
+# Die format 2D6, 1D10, 20D100
+DIE_DEF: re = re.compile(r'\d+d\d+', re.IGNORECASE)
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    show_work = False
+
+    if 'ping' in event:
+        return {'output': 'pong'}
+
+    if 'show_work' in event:
+        show_work = event['show_work']
+
+    die_counts = parse_die(event['die_count'])
+
+    if show_work:
+        return {
+            'statusCode': 200,
+            'output': json.dumps(
+                [item for sublist in list([roll_die(d, show_work) for d in die_counts]) for item in sublist])
+        }
+    return {
+        'statusCode': 200,
+        'output': json.dumps(sum([roll_die(d) for d in die_counts]))
+    }
+
+
+def parse_die(die_count: str):
+    if not die_count:
+        raise RuntimeError("Properly formatted die definitions not found (ex. '1D2,2D4')")
+
+    if DIE_DEF.match(die_count):
+        return [die for die in DIE_DEF.findall(die_count)]
+
+
+def roll_die(die_counts: str, show_work=False):
+    count, sides = die_counts.lower().split(sep='d')
+    _sum = 0
+    count = int(count)
+    sides = int(sides)
+
+    if not (count > 0 and sides > 0):
+        if show_work:
+            return [0]
+        return 0
+
+    if show_work:
+        work = []
+        for i in range(1, count + 1):
+            work.append(random.randint(1, sides))
+        return work
+
+    for i in range(1, count + 1):
+        _sum += random.randint(1, sides)
+
+    return _sum
